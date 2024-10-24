8023a480aa29fc5d08c03b4ea1dcdf0f91647214
Isabel Hernanz <isahernanz@gmail.com>
2024-10-24 17:58:30 +0200

    pito 2

--- reservas.py
+++ reservas.py
@@ -1,16 +1,14 @@
 # Este archivo definirá la clase de reservas
+import usuarios
+import json
+
 class Reserva:
-    def __init__(self, dni,
-                 nombre,
-                 apellido1,
-                 apellido2,
+    def __init__(self, usuario,
                  juego,
                  fecha_reserva,
                  fecha_finalizacion ):
-        self.dni = dni      # dni del cliente: Será el identificador del mismo
-        self.nombre = nombre        # nombre del cliente
-        self.apellido1 = apellido1       # apellido del cliente
-        self.apellido2 = apellido2
+        self. usuario = usuario    # sera un objeto de la clase usuarios
         self.juego = juego      # el juego a reservar
         self.fecha_reserva = fecha_reserva      # será la fecha de inicio de la reserva
         self.fecha_finalizacion = fecha_finalizacion        # será la fecha de finalización de la reserva
+
--- usuarios.py
+++ usuarios.py
@@ -1,4 +1,15 @@
 # Este archivo se encargará de definir la clase usuarios, con su tipo y funcionalidades
 class Usuario:
     def __init__(self, tipo):
-        self.tipo = tipo;
+        self.tipo = tipo
+
+
+class Cliente(Usuario):
+    def __init__(self, tipo, dni, nombre, apellido1, apellido2):
+        super().__init__(self)
+        self.dni = dni
+        self.nomre = nombre
+        self.apellido1 = apellido1
+        self.apellido2 = apellido2
+
+
